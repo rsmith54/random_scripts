5e6116266eb42802de037819d63c1314b6690fd1
Russell Woods Smith <russell.smith@cern.ch>
2016-07-12 18:26:15 +0200

    update to be a bit smarter

--- venv-atlas.sh
+++ venv-atlas.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-virtualenv venv
-
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-lsetup python
-lsetup root
-
-if [ -s venv/bin/activate ]
+if [ -d venv ]
 then
-    sed -i '1i setupATLAS\n' venv/bin/activate
-    sed -i '2i lsetup python\n' venv/bin/activate
-    sed -i '3i lsetup root\n' venv/bin/activate
+    echo 'venv already exists locally.  Exiting'.
+    exit
 else
-    echo 'Missing venv/bin/activate!!!'
-fi
+    virtualenv venv
 
-echo 'venv successfully started! '
-echo 'start your venv with source venv/bin/activate'
+    source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+    lsetup python
+    lsetup root
+
+    if [ -f venv/bin/activate ]
+    then
+	sed -i '1i setupATLAS\n' venv/bin/activate
+	sed -i '2i lsetup python\n' venv/bin/activate
+	sed -i '3i lsetup root\n' venv/bin/activate
+    else
+	echo 'Missing venv/bin/activate!!!'
+    fi
+
+    echo 'venv successfully started! '
+    echo 'start your venv with source venv/bin/activate'
+fi
