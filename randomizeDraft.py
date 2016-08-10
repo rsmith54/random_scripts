3ba999a1ede443c93d6c3fc5f98beb59a81908c6
Russell Smith <rws2148@columbia.edu>
2016-08-10 13:05:21 -0400

    fixing some typos

--- randomizeDraft.py
+++ randomizeDraft.py
@@ -1,7 +1,7 @@
 import random
 
 #pick a fake random seed from my mind
-random.seed(888)
+random.seed(900)
 
 #first, figure out which women's kayaker to use
 #Based on this list :
@@ -11,8 +11,8 @@ random.seed(888)
 print('Use the person who is the ith person on the page : ')
 print('https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=men%27s%20canoe%20team&mie=oly%2C%5B%22%2Fg%2F11c3ymwfzz%22%2C6%2C%22w%22%2C2%2C%22%2Fx%2F336rbv96sw9cw%22%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2Cnull%2C20%5D')
 
-womenKayaker = random.randint(0,14)
-print(womenKayaker)
+womanKayaker = random.randint(0,14)
+print(womanKayaker)
 
 #Next, we create a list and shuffle it.
 #Follow 
@@ -23,13 +23,13 @@ assert(len(listOfPeople) == 12)
 print ('List of people : ' , listOfPeople )
 print ('Match to this ordered list of teams : ')
 
-teams = [str(womenKayeker) , 'poland','us','suisse','russia','brazil','slovenia','germany','cz republic','great britain','france','slovakia']
+teams = [str(womanKayaker) , 'poland','us','suisse','russia','brazil','slovenia','germany','cz republic','great britain','france','slovakia']
 
 print('Randomizing ')
-listOfPeople.shuffle()
+random.shuffle(listOfPeople)
 
 dictOfFinalResults = dict(zip( listOfPeople , teams ))
 print("Final team mappings :")
 
-for k,v in dictOfFinalResults :
+for k,v in dictOfFinalResults.iteritems() :
     print(k,v)
